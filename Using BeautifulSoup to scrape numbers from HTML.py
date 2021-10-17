ce775cde15625f8775db7419a6a6b312f280ae17
Ayesha Sahar <83140034+AyeshaSahar@users.noreply.github.com>
2021-10-17 14:55:18 +0500

    Update Using BeautifulSoup to scrape numbers from HTML.py

--- Using BeautifulSoup to scrape numbers from HTML.py
+++ Using BeautifulSoup to scrape numbers from HTML.py
@@ -7,8 +7,7 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-# url: http://py4e-data.dr-chuck.net/comments_135852.html
-url = input('Enter - ')
+url = input('Enter: ')
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
@@ -19,4 +18,3 @@ for span in spans:
     numbers.append(int(span.string))
 
 print (sum(numbers))
-# Answer is 2565
